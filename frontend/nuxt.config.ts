1095dde3beaf3c2b103f57dc8b5960d8509caf02
Nioron07 <matthew.c.hirstius@gmail.com>
2025-10-23 14:53:01 -0400

    adding models with lfs

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: false },
-  ssr: false, // Disable SSR for static generation
+  ssr: true, // Disable SSR for static generation
   modules: [
     '@nuxt/eslint',
     'vuetify-nuxt-module',
